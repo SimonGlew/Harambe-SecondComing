72c2b77ad6272db0427490b8b81d4b304f09ce4b
slaterjack <jacktheslater@gmail.com>
2016-09-21 18:51:51 +1200

    Game logic beginning

---
+++ src/gameObjects/Item.java
@@ -0,0 +1,16 @@
+package gameObjects;
+
+public abstract class Item extends GameObject {
+	
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
---
+++ src/util/Position.java
@@ -0,0 +1,23 @@
+package util;
+
+public class Position {
+	private final int x;
+	private final int y;
+	
+	public Position(int x, int y){		
+		this.x = x;
+		this.y = y;		
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+	
+	public String toString(){
+		return "(" + x + ","+y+")";	
+	}
+}
