336c8ff57cb884a32c690d575611882672cd46b1
slaterjack <jacktheslater@gmail.com>
2016-09-23 13:57:32 +1200

    added neighbours map to locations

--- src/core/Location.java
+++ src/core/Location.java
@@ -1,14 +1,19 @@
 package core;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import tile.Tile;
 
 public class Location {
 	private Tile[][] tiles;
 	private String name;
+	private Map <GameSystem.Direction, Location> neighbours;
 	
 	public Location(String name, Tile[][] tiles){
 		this.tiles = tiles;
 		this.name = name;
+		this.neighbours = new HashMap<GameSystem.Direction, Location>();
 	}
 
 	public Tile[][] getTiles() {
