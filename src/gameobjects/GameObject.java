a27708ba658b567debbef87b8e7a735cbe2216ca
slaterjack <jacktheslater@gmail.com>
2016-09-23 14:19:26 +1200

    removed link to tile from game objects and added it just for players

--- src/gameobjects/GameObject.java
+++ src/gameobjects/GameObject.java
@@ -5,16 +5,7 @@ import java.awt.image.BufferedImage;
 import tile.Tile;
 
 public abstract class GameObject {
-	protected Tile tile;
 	protected BufferedImage image;
-
-	public Tile getTile() {
-		return tile;
-	}
-
-	public void setTile(Tile tile) {
-		this.tile = tile;
-	}
 
 	public BufferedImage getImage(){
 		return image;
