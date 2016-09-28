b1edbb971a91a8fcd004acc05fd5b99663ac26c7
slaterjack <jacktheslater@gmail.com>
2016-09-28 22:03:45 +1300

    ServerController basic shell added

--- src/clientserver/ServerController.java
+++ src/clientserver/ServerController.java
@@ -1,17 +1,73 @@
 package clientserver;
 
+import java.util.Scanner;
+
 import core.GameSystem;
+import core.GameSystem.Direction;
+import exceptions.ParserException;
+import gameobjects.Player;
 
 public class ServerController {
 	private GameSystem gameSystem;
-	
-	public ServerController (GameSystem gameSystem){
-		this.gameSystem = gameSystem;		
+
+	public ServerController(GameSystem gameSystem) {
+		this.gameSystem = gameSystem;
+	}
+
+	public boolean parseInput(ChatMessage message) {
+		Scanner s = new Scanner(message.getMessage());
+
+		if (s.hasNext()) {
+			if (s.next().equals("move")) {
+				return parseMoveCommand(s);
+			}
+
+			s.close();
+			return false;
+		}
+
+		s.close();
+		return false;
+	}
+
+	public boolean parseMoveCommand(Scanner s){
+		try{
+		 Player player = getPlayerByUserName(s.next());
+		 Direction direction = convertToDirection(s.next());
+		 
+		 if(direction == null)return false;
+		 if(player == null)return false;		 
+		 		 
+		 gameSystem.movePlayer(player, direction);
+		 return true;
+		 
+		}catch(Exception e){
+			return false;
+		}
 	}
 	
-	public boolean parseInput (ChatMessage input){
-		
-		
-		return false;		
+	public Direction convertToDirection(String s){
+		if(s.toLowerCase().equals("north")){
+			return Direction.NORTH;
+		}
+		if(s.toLowerCase().equals("south")){
+			return Direction.SOUTH;
+		}
+		if(s.toLowerCase().equals("east")){
+			return Direction.EAST;
+		}
+		if(s.toLowerCase().equals("west")){
+			return Direction.WEST;
+		}
+		return null;
+	}
+	
+	public Player getPlayerByUserName(String name){
+		for(String s : gameSystem.getPlayers().keySet()){
+			if(s.equals(name)){
+				return gameSystem.getPlayers().get(s);
+			}
+		}
+		return null;
 	}
 }
