ee99d2ba16f45faa4c4ac143dae08a23a0c7be1d
slaterjack <jacktheslater@gmail.com>
2016-09-28 21:24:12 +1300

    added ServerController class, still empty

---
+++ src/clientserver/ServerController.java
@@ -0,0 +1,17 @@
+package clientserver;
+
+import core.GameSystem;
+
+public class ServerController {
+	private GameSystem gameSystem;
+	
+	public ServerController (GameSystem gameSystem){
+		this.gameSystem = gameSystem;		
+	}
+	
+	public boolean parseInput (ChatMessage input){
+		
+		
+		return false;		
+	}
+}
