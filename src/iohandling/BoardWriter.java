c86aa68b47ca56757c58df8beb51cd9041bde1ec
jonathancarr <jonathancnz@hotmail.com>
2016-09-29 17:28:24 +1300

    Created BoardWriter method that converts board to String.

--- src/iohandling/BoardWriter.java
+++ src/iohandling/BoardWriter.java
@@ -17,9 +17,9 @@ public class BoardWriter {
 				print.println("Location");
 				print.println("id:" + loc.getId());
 				print.println("name:" + loc.getName());
-				print.println("w:"+loc.getTiles().length);
-				print.println("h:"+loc.getTiles()[0].length);
-				for(Direction dir: loc.getNeighbours().keySet()){
+				print.println("w:" + loc.getTiles().length);
+				print.println("h:" + loc.getTiles()[0].length);
+				for (Direction dir : loc.getNeighbours().keySet()) {
 					print.println(dir + ":" + loc.getNeighbours().get(dir));
 				}
 				for (int i = 0; i < loc.getTiles().length; i++) {
@@ -39,5 +39,34 @@ public class BoardWriter {
 		} catch (IOException e) {
 
 		}
+	}
+
+	public static String writeBoardToString(Board b) {
+
+		StringBuilder print = new StringBuilder();
+		for (Location loc : b.getLocations().values()) {
+			print.append("Location" + "\n");
+			print.append("id:" + loc.getId() + "\n");
+			print.append("name:" + loc.getName() + "\n");
+			print.append("w:" + loc.getTiles().length + "\n");
+			print.append("h:" + loc.getTiles()[0].length + "\n");
+			for (Direction dir : loc.getNeighbours().keySet()) {
+				print.append(dir + ":" + loc.getNeighbours().get(dir) + "\n");
+			}
+			for (int i = 0; i < loc.getTiles().length; i++) {
+				for (int j = 0; j < loc.getTiles()[0].length; j++) {
+					print.append(loc.getTiles()[j][i].toString());
+					if (loc.getTiles()[j][i].getGameObject() != null) {
+						print.append(":");
+						print.append(loc.getTiles()[j][i].getGameObject().toString());
+					}
+					print.append(",");
+
+				}
+				print.append("\n");
+			}
+		}
+		return print.toString();
+
 	}
 }
