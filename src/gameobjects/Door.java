6bf898f3d050e521e1757fe6bb0b3db13d767183
jonathancarr <jonathancnz@hotmail.com>
2016-10-10 21:13:33 +1300

    Fixed rotation of DoorOutTiles

--- src/gameobjects/Door.java
+++ src/gameobjects/Door.java
@@ -7,14 +7,14 @@ import javax.imageio.ImageIO;
 
 import util.Position;
 
-public class Door extends GameObject{
-	
+public class Door extends GameObject {
+
 	int locationID;
 	int code;
 	Position doorPosition;
 
-	public Door(int code, int locationid){
-		this.doorPosition = new Position(0,5);
+	public Door(int code, int locationid) {
+		this.doorPosition = new Position(5, 9);
 		this.code = code;
 		this.locationID = locationid;
 		try {
@@ -23,11 +23,15 @@ public class Door extends GameObject{
 			e.printStackTrace();
 		}
 	}
-	
-	public String getDescription(){
+
+	public String getDescription() {
 		return "Damian McKenzie is bae";
 	}
-	
+
+	public Position getDoorPosition() {
+		return doorPosition;
+	}
+
 	public String toString() {
 		return "Door(" + code + "," + locationID + "," + doorPosition.getX() + "," + doorPosition.getY() + ")";
 	}
