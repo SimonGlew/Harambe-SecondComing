61867452f2c54c3e589f1400145af7c2e11d742b
glewsimo <glewsimo@ecs.vuw.ac.nz>
2016-10-11 13:57:18 +1300

    jdoc parserException

--- src/exceptions/ParserException.java
+++ src/exceptions/ParserException.java
@@ -1,5 +1,10 @@
 package exceptions;
 
+/**
+ * Class that holds the the exception that gets thrown when something fails in the parser
+ * 
+ * @author Jack Slater
+ */
 public class ParserException extends Exception {
 	public ParserException(String message){
 		super(message);
